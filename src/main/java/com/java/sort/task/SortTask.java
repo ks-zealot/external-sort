4d0506cb98f548905ee7fb0cfe1e15954a98f397
zealot <y.lybarskiy@a1-systems.com>
2015-12-25 19:15:35 +0300

    fix

--- src/main/java/com/java/sort/task/SortTask.java
+++ src/main/java/com/java/sort/task/SortTask.java
@@ -35,7 +35,8 @@ public class SortTask implements Runnable {
                 public void accept(String s) {
                     try {
                         log.info("write string " + s + " to " + Paths.get(Sorter.dir , Thread.currentThread().getName(), "tempfile"));
-                        Files.write(Paths.get(Sorter.dir, Thread.currentThread().getName(), "tempfile"), s.getBytes(), StandardOpenOption.APPEND);
+                        Files.write(Paths.get(Sorter.dir, Thread.currentThread().getName() + "tempfile"), s.getBytes(),
+                                StandardOpenOption.APPEND, StandardOpenOption.CREATE);
                     } catch (IOException e) {
                         log.error("error", e);
                     }
